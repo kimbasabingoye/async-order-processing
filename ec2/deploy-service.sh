8f4157b56aa2d7734b56621b1a66757da2636113
Kimba SABI N'GOYE <kimbasabingoye@gmail.com>
2024-03-18 16:18:30 +0100

    Added minikube start step

--- ec2/deploy-service.sh
+++ ec2/deploy-service.sh
@@ -2,26 +2,28 @@
 
 cd $PROJECT_HOME/k8s
 
+NAME_SPACE='asyncop-project'
+
 # Apply Kubernetes resources from the 'namespace/' directory
 kubectl apply -f namespace/
 
 # Apply Kubernetes resources from the 'code' directory
-kubectl apply -f code --namespace=asyncop-project
+kubectl apply -f code --namespace=$NAME_SPACE
 
 # Apply MongoDB resources
-kubectl apply -f mongo/ --namespace=asyncop-project
+kubectl apply -f mongo/ --namespace=$NAME_SPACE
 
 # Apply Redis resources
-kubectl apply -f redis/ --namespace=asyncop-project
+kubectl apply -f redis/ --namespace=$NAME_SPACE
 
 # Apply RabbitMQ resources
-kubectl apply -f rabbitmq/  --namespace=asyncop-project
+kubectl apply -f rabbitmq/  --namespace=$NAME_SPACE
 
 # Apply Celery resources
-kubectl apply -f celery/  --namespace=asyncop-project
+kubectl apply -f celery/  --namespace=$NAME_SPACE
 
 # Apply Nginx resources
-kubectl apply -f nginx/  --namespace=asyncop-project
+kubectl apply -f nginx/  --namespace=$NAME_SPACE
 
 # wait
 echo "Wait for deployment complete"
@@ -30,6 +32,8 @@ sleep 30
 # Get the URL of the nginx-service and extract the port
 URL=$(minikube service nginx-service --url -n asyncop-project)
 PORT=$(echo "$URL" | cut -d ':' -f 3)
+
+kubectl port-forward --address 0.0.0.0 services/nginx-service ${PORT}:80
 
 # Set up an SSH tunnel to access the service
 ssh -i ~/.minikube/machines/minikube/id_rsa docker@$(minikube ip) -L *:${PORT}:0.0.0.0:${PORT}
--- ec2/initial_setup.sh
+++ ec2/initial_setup.sh
@@ -3,7 +3,7 @@
 echo "############ Install docker #############"
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -14,7 +14,11 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+sudo groupadd docker
+
+sudo usermod -aG docker $USER
 
 echo "############ End Install docker #############"
 
@@ -22,6 +26,14 @@ echo "############ End Install docker #############"
 echo "############ Install kubectl #############"
 
 sudo snap install kubectl --classic
+
+sudo apt-get install bash-completion -y
+
+kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null
+
+echo 'alias k=kubectl' >>~/.bashrc
+
+source ~/.bashrc
 
 echo "############ End Install kubectl #############"
 
@@ -31,6 +43,8 @@ curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/miniku
   && chmod +x minikube
 sudo mkdir -p /usr/local/bin/
 sudo install minikube /usr/local/bin/
+minikube start
+
 echo "############ End Install minikube #############"
 
 
